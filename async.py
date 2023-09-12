822c911bbcbb38446b13cd7d3e24144c136de63b
YunHaaaa <qlxqlrt2012@gmail.com>
2023-09-13 05:41:31 +0900

    Feat: async test

--- async.py
+++ async.py
@@ -1,5 +1,3 @@
-import requests
-import random
 import time
 from fastapi import FastAPI
 
@@ -21,11 +19,3 @@ async def read_results(something:str):
     s1 = await some_library(5, something)
     return {"data" : "data", "s1" : s1}
 
-
-url = "http://127.0.0.1:8081/"
-params = {'something' : f'{random.randint(0, 100)}'}
-
-res = requests.post(url, params=params)
-
-print("status : ", res.status_code)
-print(res.json())
