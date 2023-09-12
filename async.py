3ae90c78f45452daa864399a3327d04e8bd0d609
YunHaaaa <qlxqlrt2012@gmail.com>
2023-09-13 05:47:32 +0900

    Feat: change time.sleep to asyncio.sleep

--- async.py
+++ async.py
@@ -1,4 +1,5 @@
-import time
+import asyncio
+# import time
 from fastapi import FastAPI
 
 
@@ -6,8 +7,10 @@ async def some_library(num:int, something:str):
     s = 0
     for i in range(num):
         print("something : ", something, i)
-        time.sleep(1)
-        s += 1
+        # time.sleep : 동기적으로 작동하는 함수
+        # asyncio.sleep : 비동기적으로 작동하는 함수
+        await asyncio.sleep(1)
+        s += int(something)
     return s
 
 
