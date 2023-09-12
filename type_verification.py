2d11ade6162c4b6901b583e42b3df454f57a568d
YunHaaaa <qlxqlrt2012@gmail.com>
2023-09-13 06:13:29 +0900

    Feat: length constraint

--- type_verification.py
+++ type_verification.py
@@ -1,7 +1,7 @@
 from typing import List, Optional, Union
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Movie(BaseModel):
     mid: int
@@ -11,8 +11,19 @@ class Movie(BaseModel):
     date: Optional[datetime]
     some_variable_list: List[int] = [] # int로 구성된 리스트, 기본값은 []
 
+class User(BaseModel):
+    '''
+    gt : 설정된 값보다 큰
+    ge : 설정된 값보다 크거나 작은
+    lt : 설정된 값보다 작은
+    le : 설정된 값보다 작거나 같은
+    '''
+    uid: int
+    name: str = Field(min_length=2, max_length=7)
+    age: int = Field(gt=1, le=130)
 
-tmp_data = {
+
+tmp_movie_data = {
     'mid' : '1',
     'genre' : 'action',
     'rate' : 1.5,
@@ -20,5 +31,13 @@ tmp_data = {
     'date' : '2023-01-03 19:12:11'
 }
 
-tmp_movie = Movie(**tmp_data)
-print(tmp_movie)
+tmp_user_data = {
+    'uid' : '100',
+    'name' : 'soojin',
+    'age' : '12'
+}
+
+tmp_movie = Movie(**tmp_movie_data)
+tmp_user = User(**tmp_user_data)
+print(tmp_movie.model_dump_json())
+print(tmp_user.model_dump_json())
